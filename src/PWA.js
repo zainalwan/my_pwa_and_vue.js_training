d720e8b6a81d1300f1f9c7748c288f677dc9ccc4
zainalwan <zainalwan4@gmail.com>
2021-06-27 18:28:06 +0700

    Create PWA.js

---
+++ src/PWA.js
@@ -0,0 +1,16 @@
+export default {
+    init()
+    {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service_worker.js')
+                    .then(function(registration) {
+                        console.log('ServiceWorker registration successful with scope: ',
+                            registration.scope);
+                    }, function(err) {
+                        console.log('ServiceWorker registration failed: ', err);
+                    });
+            });
+        }
+    }
+}
